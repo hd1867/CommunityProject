9145bbc90ee343105f91fce43bcdefd63950ba4e
Hendrick Ducasse <hd1867@rit.edu>
2020-02-08 12:56:03 -0500

    Added Bootstrap

--- app.py
+++ app.py
@@ -1,13 +1,53 @@
-from flask import Flask, render_template
+import os
+from datetime import timedelta
+
+from flask import Flask, render_template, request, redirect, flash, url_for, session
 from flask_bootstrap import Bootstrap
 
+from utils import databaseUtils
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 Bootstrap(app)
+
 
 @app.route('/')
 def root():
     return render_template("home.html")
+
+
+@app.route('/login')
+def posts():
+    return render_template("login.html")
+
+
+@app.route("/auth", methods=["POST"])
+def auth():
+    if "submit" not in request.form or "user" not in request.form or "pwd" not in request.form:
+        flash("At least one form input was incorrect")
+        return redirect(url_for('login'))
+
+    if request.form['submit'] == 'Login':
+        user = databaseUtils.authenticate(request.form['user'], request.form['pwd'])
+        if user:
+            session['user'] = str(user)
+            session.permanent = True
+            app.permanent_session_lifetime = timedelta(minutes=30)
+            return redirect(url_for('root'))
+        else:
+            flash('Incorrect username or password')
+            return redirect(url_for('login'))
+    else:
+        success = databaseUtils.create_user(request.form['user'], request.form['pwd'])
+        if (success):
+            session['user'] = str(success)
+            session.permanent = True
+            app.permanent_session_lifetime = timedelta(minutes=30)
+            return redirect(url_for('root'))
+        else:
+            flash('This username already exists!')
+            return redirect(url_for('login'))
+
 
 
 if __name__ == '__main__':
